a792b98bf944c344d6384795791c2b75bd9c397f
Yurii Soroka <yurisoroka165@gmail.com>
2023-02-25 18:00:56 +0200

    key did not add to li

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,10 +1,24 @@
+import PropTypes from "prop-types";
 import css from "./FriendList.module.css";
 import { FriendListItem } from "components/FriendListItem/FriendListItem";
 
 export const FriendList = ({ friends }) => {
     return (
-        <ul className={css.friend_list}>
-            <FriendListItem friends={friends} />
-        </ul>
+      <ul className={css.friend_list}>
+        {friends.map(friend => (
+          <>
+            <FriendListItem
+              avatar={friend.avatar}
+              name={friend.name}
+              isOnline={friend.isOnline}
+              id={friend.id}
+            />
+          </>
+        ))}
+      </ul>
     );
 };
+
+// FriendList.propTypes = {
+//   friends: PropTypes.array,
+// };
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,28 +1,24 @@
-import PropTypes from "prop-types";
-import css from "./FriendListItem.module.css";
+import PropTypes from 'prop-types';
+import css from './FriendListItem.module.css';
 
-export const FriendListItem = ({ friends }) => {
+export const FriendListItem = ({ avatar, name, isOnline, id }) => {
     return (
-        <>
-            {friends.map(({ avatar, name, isOnline, id }) => (
-                <li key={id} className={css.item}>
-                    <span className={`${css.status} ${css[isOnline]}`}></span>
-                    <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-                    <p className={css.name}>{name}</p>
-                </li>
-            ))}
-        </>
+        <li key={id} className={css.item}>
+          <span className={`${css.status} ${css[isOnline]}`}></span>
+          <img
+            className={css.avatar}
+            src={avatar}
+            alt="User avatar"
+            width="48"
+          />
+          <p className={css.name}>{name}</p>
+        </li>
     );
 };
 
-
-FriendListItem.propTypes = {
-    friends: PropTypes.arrayOf(
-        PropTypes.exact({
-            avatar: PropTypes.string,
-            name: PropTypes.string,
-            isOnline: PropTypes.bool,
-            id: PropTypes.number
-        }),
-    ),
-};
+// FriendListItem.propTypes = {
+//   avatar: PropTypes.string.isRequired,
+//   name: PropTypes.string.isRequired,
+//   isOnline: PropTypes.bool.isRequired,
+//   id: PropTypes.number.isRequired
+// };
